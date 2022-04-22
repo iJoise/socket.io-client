b2ee982d0dde13160ec5491c9bf84cd59cfa38f6
Кирилл Кондратьев <kondratevk.1989@mail.ru>
2022-04-22 13:53:13 +0700

    пробное соединение по ws с беком

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { io, Socket } from 'socket.io-client';
+
+type Messages = {
+  message: string
+  id: string
+  user: {
+    id: string
+    name: string
+  }
+}
 
 function App() {
+  const socketRef = useRef<Socket>();
+  const [messages, setMessages] = useState<Messages[] | null>(null);
+  const [mess, setMess] = useState('hello');
+
+  useEffect(() => {
+    socketRef.current = io('http://localhost:5000', {
+      transports: ['websocket', 'polling'],
+    });
+
+    socketRef.current?.on('init-messages-published', (messages) => {
+      setMessages(messages)
+    })
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={'chat'}>
+        {messages && messages.map(m => (
+          <div key={m.id}>
+            <b>{m.user.name}: </b>
+            {m.message}
+            <hr />
+          </div>
+        ))}
+      </div>
+      <textarea
+        name="chat"
+        id="chat"
+        cols={35}
+        rows={5}
+        value={mess}
+        onChange={e => setMess(e.currentTarget.value)}
+      />
+      <button
+        onClick={() => {
+          socketRef.current?.emit('client-message-sent', mess);
+          setMess('')
+        }}
+      >
+        Send
+      </button>
     </div>
   );
 }
